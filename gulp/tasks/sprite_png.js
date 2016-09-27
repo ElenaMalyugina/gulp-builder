a839ac0adb2cf831d6b034960f8e8527bed59d13
Elena Malyugina <malyugina.el@yandex.ru>
2016-09-27 08:40:48 +0300

    Добвалено разделение img и css-потока в sprite_png

--- gulp/tasks/sprite_png.js
+++ gulp/tasks/sprite_png.js
@@ -2,10 +2,11 @@
 
 module.exports = function(){
 	$.gulp.task('sprite_png', function(){
- 	var spriteData = $.gulp.src('././source/images/*.png').pipe($.gp.spritesmith({
+ 	var spriteData = $.gulp.src('././source/images/images_for_sprite/*.png').pipe($.gp.spritesmith({
     	imgName: 'sprite.png',
-    	cssName: 'sprite.css'
+    	cssName: 'sprite.scss'
   }));
-  return spriteData.pipe($.gulp.dest('././build/assets/img'));
+  return spriteData.img.pipe($.gulp.dest('././build/assets/img')),
+	  spriteData.css.pipe($.gulp.dest('././source/style'));
 })
 };
